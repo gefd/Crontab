5c20cf02a8d24f6f1bf7798b19f014971c9839f3
Geoff Davis <gef.davis@gmail.com>
2016-06-06 20:28:12 +1000

    Remove the seperate special crontab test file, and update unit tests

--- tests/Crontab/Tests/CrontabFileHandlerTest.php
+++ tests/Crontab/Tests/CrontabFileHandlerTest.php
@@ -30,16 +30,10 @@ class CrontabFileHandlerTest extends \PHPUnit_Framework_TestCase
      */
     private $fixtureFile;
 
-    /**
-     * @var string with the path to the fixture file
-     */
-    private $fixtureSpecialsFile;
-
     public function setUp()
     {
         $fixturesDir = __DIR__.'/../../fixtures';
         $this->fixtureFile = $fixturesDir."/crontab";
-        $this->fixtureSpecialsFile = $fixturesDir."/crontab.specials";
         $this->tempFile = tempnam(sys_get_temp_dir(), 'cron');
         $this->crontabFileHandler = new CrontabFileHandler();
         $this->crontab = new Crontab();
@@ -58,7 +52,7 @@ class CrontabFileHandlerTest extends \PHPUnit_Framework_TestCase
     public function testParseFromFile()
     {
         $this->crontabFileHandler->parseFromFile($this->crontab, $this->fixtureFile);
-        $this->assertCount(4, $this->crontab->getJobs());
+        $this->assertCount(12, $this->crontab->getJobs());
 
         $jobs = $this->crontab->getJobs();
         $job1 = array_shift($jobs);
@@ -87,6 +81,12 @@ class CrontabFileHandlerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("MAILTO=root@localhost.localdomain", $var1->render());
         $this->assertEquals('BIN_PATH', $var2->getName());
     }
+    private function stripWhiteSpace($string)
+    {
+        return implode(PHP_EOL, array_map(function ($str) {
+            return preg_replace('/\s+/', ' ', $str);
+        }, preg_split("/$/", trim($string), null, PREG_SPLIT_NO_EMPTY)));
+    }
 
     public function testWriteToFileIsSuccessfulWhenFileIsWritable()
     {
@@ -94,7 +94,10 @@ class CrontabFileHandlerTest extends \PHPUnit_Framework_TestCase
 
         $this->crontabFileHandler->writeToFile($this->crontab, $this->tempFile);
 
-        $this->assertSame($this->crontab->render().PHP_EOL, file_get_contents($this->tempFile));
+        $fixtureContents = $this->stripWhiteSpace(file_get_contents($this->fixtureFile));
+        $fileContents = $this->stripWhiteSpace(file_get_contents($this->tempFile));
+
+        $this->assertSame($fixtureContents, $fileContents);
     }
 
     public function testWriteToFileContainsVariables()
@@ -102,14 +105,10 @@ class CrontabFileHandlerTest extends \PHPUnit_Framework_TestCase
         $this->crontabFileHandler->parseFromFile($this->crontab, $this->fixtureFile);
         $this->crontabFileHandler->writeToFile($this->crontab, $this->tempFile);
 
-        // Strip extra whitespace and blank lines from the fixture file
-        $fixtureLines = array_map('trim', file($this->fixtureFile));
-        $fixtureContents = array_map(function($line) {
-            return '' !== trim($line) ? preg_replace('/\s+/', ' ', trim($line)) : null;
-        }, $fixtureLines);
-        $fileContents = file_get_contents($this->tempFile);
+        $fixtureContents = $this->stripWhiteSpace(file_get_contents($this->fixtureFile));
+        $fileContents = $this->stripWhiteSpace(file_get_contents($this->tempFile));
 
-        $this->assertSame(implode("\n", array_filter($fixtureContents)) . PHP_EOL, $fileContents);
+        $this->assertSame($fixtureContents, $fileContents);
     }
 
     /**
@@ -125,17 +124,32 @@ class CrontabFileHandlerTest extends \PHPUnit_Framework_TestCase
         $this->crontabFileHandler->writeToFile($this->crontab, $this->tempFile);
         // Expected an InvalidArgumentException because the file is not writable.
     }
-    
+
     public function testParseFromFileWithSpecialWords()
     {
-        $this->crontabFileHandler->parseFromFile($this->crontab, $this->fixtureSpecialsFile);
+        $this->crontabFileHandler->parseFromFile($this->crontab, $this->fixtureFile);
 
-        $this->assertCount(8, $this->crontab->getJobs());
-        $specialsContents = file_get_contents($this->fixtureSpecialsFile);
-        $output = implode("\n", array_map(function($str) {
-            return preg_replace('/\s+/', ' ', $str);
-        }, explode(PHP_EOL, $this->crontab->render())));
+        $this->assertCount(12, $this->crontab->getJobs());
+
+        $specialsContents = $this->stripWhiteSpace(file_get_contents($this->fixtureFile));
+        $output = $this->stripWhiteSpace($this->crontab->render());
 
         $this->assertEquals($specialsContents, $output);
+
+        $jobs = $this->crontab->getJobs();
+        $job1 = array_shift($jobs);
+        $job2 = array_shift($jobs);
+        $job3 = array_shift($jobs);
+        $job4 = array_shift($jobs);
+
+        $this->assertInstanceOf('Crontab\\Job', $job1);
+        $this->assertInstanceOf('Crontab\\Job', $job2);
+        $this->assertInstanceOf('Crontab\\Job', $job3);
+        $this->assertInstanceOf('Crontab\\Job', $job4);
+
+        // Remaining Jobs are JobSpecial instances
+        foreach ($jobs as $job) {
+            $this->assertInstanceOf('Crontab\\JobSpecial', $job);
+        }
     }
 }
